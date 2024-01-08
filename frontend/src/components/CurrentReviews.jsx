7acad23b59649e8511b20634e932dd08f3eeca67
anyshKHDR <anyshkhdr@gmail.com>
2024-01-09 03:59:01 +0530

    'CurrentReview' code simplified

--- frontend/src/components/CurrentReviews.jsx
+++ frontend/src/components/CurrentReviews.jsx
@@ -1,48 +1,45 @@
 import React, { useState } from "react";
 
-const CurrentReviews = ({showAddAReview,user,review})=>{
-
-    console.log(user)
+const CurrentReviews = ({ showAddAReview, user, review }) => {
     const [showMore, setShowMore] = useState(new Array(review.length).fill(false));
 
-    const handleClick = (index)=>{
-        const currentState = [...showMore];
-        currentState[index] = !showMore[index];
-        setShowMore(currentState);
+    const handleClick = (index) => {
+        setShowMore(showMore.map((item, idx) => idx === index ? !item : item));
     }
-    // console.log(review);
-    
-    return(
-        !showAddAReview&& (
-            <div className="review hlt" id="rvwBox">
-                {user.map((theUser, index)=>(
-                    <div id="rvws">
-                        {review[index]!== "" &&
-                            <p 
-                                className="user"
-                                style={{fontWeight:"500",color:"#9895a4"}}
-                            >
-                                {theUser}
-                            </p>
-                        }
+    if (!showAddAReview && review.length === 0) {
+        return(
+            <div className="review hlt" id="rvwBox" style={{color:"#abb2d9"}}>
+                No Reviews
+            </div>
+        )
+    }
 
-                        {review.map((reviewText, reviewIndex) => (
-                            (reviewIndex===index)&& (
-                                <p
-                                    key={reviewIndex}
-                                    className="rvw" 
-                                    onClick={()=>handleClick(reviewIndex)} 
-                                    style={{whiteSpace:showMore[reviewIndex]&&"normal"}} 
-                                >
-                                    {reviewText}
+    return (
+        !showAddAReview && (
+            <div className="review hlt" id="rvwBox">
+                {user.map((theUser, index) => (
+                    <div key={index} id="rvws">
+                        {review[index] !== "" && (
+
+                            //shorthand for <React.Fragment> - for grouping multiple elements without adding an extra node to the DOM
+                            <> 
+                                <p className="user" style={{ fontWeight: "500", color: "#9895a4" }}>
+                                    {theUser}
                                 </p>
-                            )
-                        ))}
+                                <p
+                                    className="rvw"
+                                    onClick={() => handleClick(index)}
+                                    style={{ whiteSpace: showMore[index] ? "normal" : "nowrap" }}
+                                >
+                                    {review[index]}
+                                </p>
+                            </>
+                        )}
                     </div>
                 ))}
             </div>
         )
-    )
-}
+    );
+};
 
 export default CurrentReviews;
