b937e21177c7b05b64bf6b7c7ca3c66e45438953
anyshKHDR <anyshkhdr@gmail.com>
2024-01-16 00:06:05 +0530

    Replaced ReactDOM.render with createRoot for React 18 compatibility

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,10 @@
 import React, { StrictMode } from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client"
 import App from "./components/App";
 
 
-ReactDOM.render(<StrictMode><App /></StrictMode> , document.getElementById("root"));
+// ReactDOM.render(<StrictMode><App /></StrictMode> , document.getElementById("root"));
+
+const root = createRoot(document.getElementById("root"));
+root.render(<StrictMode> <App /> </StrictMode>);
